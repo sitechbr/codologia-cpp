f0cd91154da212322b6fee37d25c3ba6517c29ec
NestBris <sitech.br@gmail.com>
2022-08-20 22:43:39 +0300

    Function simple code

---
+++ Function.cpp
@@ -0,0 +1,19 @@
+#include <iostream>
+using namespace std;
+
+int DemoConsoleOutput();
+
+int main()
+{
+  DemoConsoleOutput();
+  return 0;
+}
+
+int DemoConsoleOutput()
+{
+    cout<<"String"<<endl;
+    cout<<"Nubmer five: " <<  5 << endl;
+    cout<<"Division: " <<  10/5 << endl;
+    cout<<"Pi: " <<  22/7 << endl;
+    cout<<"Pi: " <<  22.0/7 << endl;
+}
