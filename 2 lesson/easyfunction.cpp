0dd3e4b4207a6c8e62611820f90b99e78e073601
NestBris <sitech.br@gmail.com>
2022-08-25 21:52:48 +0300

    lesson 2 part1

---
+++ 2 lesson/easyfunction.cpp
@@ -0,0 +1,20 @@
+#include <iostream>
+using namespace std;
+
+int DemoConsoleOutput()
+{
+    cout << "Simple String" << endl;
+    cout << "Выполнение деления 10/5 = " << 10/5<< endl;
+    cout << "Число Пи примерно равно 22/7 = "<< 22/7<< endl;
+    cout << "Более точно Пи равно 22/7 = "<< 22.0/7<< endl;
+    return 0;
+
+
+}
+
+int main()
+{
+  //cout << "Hello, World!" << endl;
+  DemoConsoleOutput();
+  return 0;
+}
