7bb0f7eac01f7f9979caf44d7b19eea13bce1ab6
NestBris <63145839+sitechbr@users.noreply.github.com>
2022-09-09 21:51:32 +0300

    Create sum3num.cpp

---
+++ sum3num.cpp
@@ -0,0 +1,18 @@
+#include <iostream>
+
+using namespace std;
+
+int main()
+{
+    int a, b, c = 0;
+    cout << "Input first number" << endl;
+    cout << "Input first number" << endl;
+    cin >> a;
+    cout << "Input secord number" << endl;
+    cin >> b;
+    cout << "Input third number " << endl;
+    cin >> c;
+    cout << a + b + c;
+
+}
+
