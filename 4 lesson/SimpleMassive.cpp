6a07bd966d53318d2301d8b4321feea83259f0b6
NestBris <63145839+sitechbr@users.noreply.github.com>
2022-09-09 22:56:03 +0300

    Create SimpleMassive.cpp

---
+++ 4 lesson/SimpleMassive.cpp
@@ -0,0 +1,18 @@
+
+#include <iostream>
+using namespace std;
+
+
+int main()
+{
+    int myNumbers[5] = { 1,2,3,4,5 };
+
+    cout << myNumbers[0] << endl;
+    cout << myNumbers[1] << endl;
+    cout << myNumbers[2] << endl;
+    cout << myNumbers[3] << endl;
+    cout << myNumbers[4] << endl;
+    return 0;
+
+
+}
